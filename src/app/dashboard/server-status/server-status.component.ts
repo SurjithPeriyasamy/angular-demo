45ef6b996cdc680d7f53cb20dec1df2863540d61
Surjith <mahindhiran005@gmail.com>
2025-07-01 20:04:52 +0530

    .

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -15,35 +15,36 @@ import {
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent {
   currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
-  private destroyRef = inject(DestroyRef);
+  // private destroyRef = inject(DestroyRef);
   constructor() {
-    effect((onCleanUp) => {
-      console.log(this.currentStatus());
-    });
-  }
-  ngOnInit() {
-    console.log('onInit');
-
-    const interval = setInterval(() => {
-      const rn = Math.random();
-      if (rn < 0.3) {
-        this.currentStatus.set('online');
-      } else if (rn < 0.7) {
-        this.currentStatus.set('offline');
-      } else {
-        this.currentStatus.set('unknown');
-      }
-    }, 5000);
-    this.destroyRef.onDestroy(() => {
-      clearInterval(interval);
-    });
+    // effect((onCleanUp) => {
+    //   console.log(this.currentStatus());
+    // });
   }
 
-  ngAfterViewInit() {
-    console.log('view init');
-  }
+  // ngOnInit() {
+  //   console.log('onInit');
+
+  //   const interval = setInterval(() => {
+  //     const rn = Math.random();
+  //     if (rn < 0.3) {
+  //       this.currentStatus.set('online');
+  //     } else if (rn < 0.7) {
+  //       this.currentStatus.set('offline');
+  //     } else {
+  //       this.currentStatus.set('unknown');
+  //     }
+  //   }, 5000);
+  //   this.destroyRef.onDestroy(() => {
+  //     clearInterval(interval);
+  //   });
+  // }
+
+  // ngAfterViewInit() {
+  //   console.log('view init');
+  // }
 
   // ngOnDestroy() {
   //   clearInterval(this.interval);
