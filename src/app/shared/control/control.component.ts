9c59f0f637b812b21bfba4b3ae4f7069b62ea163
Surjith <mahindhiran005@gmail.com>
2025-07-01 18:50:00 +0530

    contentChild

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,5 +1,7 @@
 import {
   Component,
+  contentChild,
+  ContentChild,
   ElementRef,
   HostBinding,
   HostListener,
@@ -17,7 +19,7 @@ import {
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
-    // '(click)': 'onClick()',
+    '(click)': 'onClick()',
   },
 })
 export class ControlComponent {
@@ -25,10 +27,37 @@ export class ControlComponent {
   // @HostBinding('class') className = 'control';  >>>>>listening event from host is recommended
   // @HostListener('click') onClick() {
   //   console.log('clicked');
-  // }
+  // }  host Listener
   // constructor(private el: ElementRef) {}
   // private el = inject(ElementRef);
-  // onClick() {
-  //   console.log(this.el);
-  // }
+
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+
+  private control =
+    contentChild.required<ElementRef<HTMLInputElement | HTMLTextAreaElement>>(
+      'input'
+    );
+  onClick() {
+    console.log(this.control());
+  }
 }
+
+// @Component({
+//   selector: 'app-control',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './control.component.html',
+//   styleUrl: './control.component.css',
+//   encapsulation: ViewEncapsulation.None,
+// })
+// export class ControlComponent {
+//   label = input.required<string>();
+//   // @HostBinding('class') className = 'control';  >>>>>listening event from host is recommended
+//   // @HostListener('click') onClick() {
+//   //   console.log('clicked');
+//   // }  host Listener
+//   // constructor(private el: ElementRef) {}
+//   // private el = inject(ElementRef);
+// }
